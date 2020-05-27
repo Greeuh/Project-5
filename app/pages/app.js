63b15e01345822cac43a1621c8795df73e85891b
Greeuh <Greeuh@users.noreply.github.com>
2020-05-28 00:37:06 +0200

    work on TweetsTimeline

--- app/pages/app.js
+++ app/pages/app.js
@@ -1,38 +1,80 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Twitter from 'twitter-lite';
+import './timeline.css';
 
+class TweetsTimeline extends Component {
+  constructor(props) {
+    super(props)
 
-const client = new Twitter({
-  subdomain: "api", // "api" is the default (change for other subdomains)
-  version: "1.1", // version "1.1" is the default (change for other subdomains)
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+    this.state = {
+      timeline: []
+    };
+  }
 
-client
-  .get("account/verify_credentials")
-  .then(results => {
-    console.log("results", results);
-  })
-  .catch(console.error);
+  componentDidMount() {
 
-client
-  .get("statuses/home_timeline")
-  .then(results => {
-    console.log("results", results);
-  })
-  .catch(console.error);
+    const client = new Twitter({
+      subdomain: "api", // "api" is the default (change for other subdomains)
+      version: "1.1", // version "1.1" is the default (change for other subdomains)
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+
+    });
+
+    client
+      .get("account/verify_credentials")
+      .then(results => {
+        console.log("results", results);
+      })
+      .catch(console.error);
+
+    client
+      .get("statuses/home_timeline")
+      .then(results => {
+        this.setState({ timeline: results })
+      })
+      .catch(console.error);
+  }
+
+  render() {
+
+    return (
+      <div className="Timeline">
+        <div class="tw-block-parent">
+          <div class="timeline-TweetList-tweet">
+            <div class="timeline-Tweet">
+              <div class="timeline-Tweet-brand">
+                <div class="Icon Icon--twitter"></div>
+              </div>
+              <div class="timeline-Tweet-author">
+                <div class="TweetAuthor"><a class="TweetAuthor-link" href="#channel"> </a><span class="TweetAuthor-avatar">
+                  <div class="Avatar"> </div></span><span class="TweetAuthor-name">TwitterDev</span><span class="Icon Icon--verified"> </span><span class="TweetAuthor-screenName">@TwitterDev</span></div>
+              </div>
+              <div class="timeline-Tweet-text">We're excited for the inaugural Twitter Community Meetup<a href="#">@TwitterSeattle</a><span>tomorrow!</span><a href="#">#TapIntoTwitter</a><a href="#">meetup.com/Seattle-Twitte…</a></div>
+              <div class="timeline-Tweet-metadata"><span class="timeline-Tweet-timestamp">9h</span></div>
+              <ul class="timeline-Tweet-actions">
+                <li class="timeline-Tweet-action"><a class="Icon Icon--heart" href="#"></a></li>
+                <li class="timeline-Tweet-action"><a class="Icon Icon--share" href="#"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
 
 class App extends Component {
   constructor(props) {
-      super(props)
+    super(props)
 
-      this.state = {
+    this.state = {
       products: []
-      };
+    };
   }
 
 
@@ -41,39 +83,40 @@ class App extends Component {
 
     let token = localStorage.getItem('token');
 
-    axios.post('http://localhost:8000/api/login', 
-    {
+    axios.post('http://localhost:8000/api/login',
+      {
         email: 'ant@gmail.com',
         password: '123456'
-    })
-    .then(function (res) {
-      localStorage.setItem('token', res.data.data.token);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+      })
+      .then(function (res) {
+        localStorage.setItem('token', res.data.data.token);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
-    axios.get('http://localhost:8000/api/products',{
+    axios.get('http://localhost:8000/api/products', {
       headers: {
-        'Authorization' : `Bearer ${token}`
-    }})
+        'Authorization': `Bearer ${token}`
+      }
+    })
       .then(res => {
         this.setState({ products: res.data.data })
         console.log(res.data.data[0].id)
       })
   }
-  
+
   render() {
-     
+
     return (
       <div className="App">
-      <ul>
-      ils sont où les produits ?
+        <ul>
+          ils sont où les produits ?
        {this.state.products.filter(product => product.id < 10).map(product => <div>
-       <p>{product.id}</p>
-       <p>{product.name}</p>
-       </div>)}
-      </ul>
+          <p>{product.id}</p>
+          <p>{product.name}</p>
+        </div>)}
+        </ul>
       </div>
     );
   }
