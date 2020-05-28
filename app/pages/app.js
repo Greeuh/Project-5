6ee4cd404107c45e86a0789e5ada9e5473edf6d9
Greeuh <Greeuh@users.noreply.github.com>
2020-05-28 17:51:41 +0200

    Update app.js

--- app/pages/app.js
+++ app/pages/app.js
@@ -32,6 +32,7 @@ class TweetsTimeline extends Component {
 
     client
       .get("statuses/home_timeline")
+      .then(response => response.json())
       .then(results => {
         console.log(results)
         this.setState({ tweets: results })
