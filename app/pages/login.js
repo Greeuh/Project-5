f4fbfe41e920856b1a7291a3ea897b6ea9711c3c
Greeuh <Greeuh@users.noreply.github.com>
2020-06-04 18:30:46 +0200

    Update login.js

--- app/pages/login.js
+++ app/pages/login.js
@@ -12,11 +12,12 @@ class Login extends Component {
         client
             .getRequestToken("https://projet5ocr.antoineparriaud.fr:3000/authorization")
             .then(res => {
+                let token = res.oauth_token
                 console.log({
                     reqTkn: res.oauth_token,
                     reqTknSecret: res.oauth_token_secret
                 })
-                window.location.replace("https://api.twitter.com/oauth/authorize?" + res.oauth_token);
+                window.location.replace("https://api.twitter.com/oauth/authorize?" + token);
             })
             .catch(console.error);
      }
