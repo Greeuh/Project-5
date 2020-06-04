75cbc01a904cccbbaf84335da25adf8ab020bdbe
Greeuh <Greeuh@users.noreply.github.com>
2020-06-04 16:48:31 +0200

    add keys to log/auth.js

--- app/pages/authorization.js
+++ app/pages/authorization.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import Twitter from 'twitter-lite';
 
 const client = new Twitter({
-    consumer_key: "xyz",
-    consumer_secret: "xyz"
-  });
-   
-  client
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+});
+
+client
     .getAccessToken({
-      oauth_verifier: oauthVerifier,
-      oauth_token: oauthToken
+        oauth_verifier: oauthVerifier,
+        oauth_token: oauthToken
     })
     .then(res =>
-      console.log({
-        accTkn: res.oauth_token,
-        accTknSecret: res.oauth_token_secret,
-        userId: res.user_id,
-        screenName: res.screen_name
-      })
+        console.log({
+            accTkn: res.oauth_token,
+            accTknSecret: res.oauth_token_secret,
+            userId: res.user_id,
+            screenName: res.screen_name
+        })
     )
     .catch(console.error);
--- app/pages/login.js
+++ app/pages/login.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import Twitter from 'twitter-lite';
 
 const client = new Twitter({
-    consumer_key: "xyz",
-    consumer_secret: "xyz"
-  });
-   
-  client
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+});
+
+client
     .getRequestToken("https://projet5ocr.antoineparriaud.fr:3000/authorization")
     .then(res =>
-      console.log({
-        reqTkn: res.oauth_token,
-        reqTknSecret: res.oauth_token_secret
-      })
+        console.log({
+            reqTkn: res.oauth_token,
+            reqTknSecret: res.oauth_token_secret
+        })
     )
     .catch(console.error);
