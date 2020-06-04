02956567f413aca864fdb73adb0f35c5039e798e
Greeuh <Greeuh@users.noreply.github.com>
2020-06-04 17:37:02 +0200

    Update auth.js & log.js

--- app/pages/authorization.js
+++ app/pages/authorization.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import Twitter from 'twitter-lite';
 
-const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-});
 
-client
-    .getAccessToken({
-        oauth_verifier: oauthVerifier,
-        oauth_token: oauthToken
-    })
-    .then(res =>
-        console.log({
-            accTkn: res.oauth_token,
-            accTknSecret: res.oauth_token_secret,
-            userId: res.user_id,
-            screenName: res.screen_name
+class Authorization extends Component {
+
+    componentDidMount (){
+    const client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    });
+
+    client
+        .getAccessToken({
+            oauth_verifier: oauthVerifier,
+            oauth_token: oauthToken
         })
-    )
-    .catch(console.error);
+        .then(res =>
+            console.log({
+                accTkn: res.oauth_token,
+                accTknSecret: res.oauth_token_secret,
+                userId: res.user_id,
+                screenName: res.screen_name
+            })
+        )
+        .catch(console.error);
+    }
+    
+}
+
+export default Authorization;
--- app/pages/login.js
+++ app/pages/login.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import Twitter from 'twitter-lite';
 
-const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-});
+class Login extends Component {
 
-client
-    .getRequestToken("https://projet5ocr.antoineparriaud.fr:3000/authorization")
-    .then(res =>
-        console.log({
-            reqTkn: res.oauth_token,
-            reqTknSecret: res.oauth_token_secret
-        })
-    )
-    .catch(console.error);
+    componentDidMount() {
+        const client = new Twitter({
+            consumer_key: process.env.TWITTER_CONSUMER_KEY,
+            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        });
+
+        client
+            .getRequestToken("https://projet5ocr.antoineparriaud.fr:3000/authorization")
+            .then(res =>
+                console.log({
+                    reqTkn: res.oauth_token,
+                    reqTknSecret: res.oauth_token_secret
+                })
+            )
+            .catch(console.error);
+        window.location.replace("/authorization");
+    }
+}
+
+export default Login;
