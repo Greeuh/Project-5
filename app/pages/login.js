05889fe047c0b9607d1174e06ec6bd7d12093649
Greeuh <Greeuh@users.noreply.github.com>
2020-06-04 19:18:41 +0200

    Update login.js

--- app/pages/login.js
+++ app/pages/login.js
@@ -18,15 +18,15 @@ class Login extends Component {
 
         client
             .getRequestToken("https://projet5ocr.antoineparriaud.fr:3000/authorization")
-            .then(res => {
-                this.setState({ token: res.oauth_token })
-                document.getElementById("link").href="https://api.twitter.com/oauth/authorize?" + res.oauth_token; 
+            .then(res => 
+                // this.setState({ token: res.oauth_token })
+                // document.getElementById("link").href="https://api.twitter.com/oauth/authorize?" + res.oauth_token; 
                 console.log({
                     reqTkn: res.oauth_token,
                     reqTknSecret: res.oauth_token_secret
                 })
                 // window.location.replace("https://api.twitter.com/oauth/authorize?" + res.oauth_token);
-            })
+            )
             .catch(console.error);
      }
 
@@ -35,7 +35,7 @@ class Login extends Component {
         <div>
             <h1>Vous allez être redirigé sur le site de Twitter...</h1>
             <a href='' id="link" >Ou cliquez là </a>
-            <p>Ceci est le token : {this.state.token} .</p>
+            <p>Ceci est le token : {this.state.token.res.oauth_token} .</p>
         </div>);
     }
 }
