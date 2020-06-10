c620950e7a4ecb45af9d8dd631a0e0ea66772997
Greeuh <Greeuh@users.noreply.github.com>
2020-06-10 20:18:06 +0200

    Update authorization.js

--- app/pages/authorization.js
+++ app/pages/authorization.js
@@ -2,44 +2,47 @@ import React, { Component } from 'react';
 import Twitter from 'twitter-lite';
 
 
-class Authorization extends Component {
+function Authorization({ name }) {
+    return (
+        <div>
+            <h1>Page d'authentification</h1>
+            <h2>Bienvenue {name}</h2>
+        </div>
+    )
+}
 
-    componentDidMount() {
-        const client = new Twitter({
-            consumer_key: process.env.TWITTER_CONSUMER_KEY,
-            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        });
+export async function getServerSideProps() {
 
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const oauthVerifier = urlParams.get('oauth_verifier');
-        const oauthToken = urlParams.get('oauth_token');
+    const client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    });
+
+    let name;
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const oauthVerifier = urlParams.get('oauth_verifier');
+    const oauthToken = urlParams.get('oauth_token');
 
 
-            client
-                .getAccessToken({
-                    oauth_verifier: oauthVerifier,
-                    oauth_token: oauthToken
-                })
-                .then(res =>
-                    console.log({
-                        accTkn: res.oauth_token,
-                        accTknSecret: res.oauth_token_secret,
-                        userId: res.user_id,
-                        screenName: res.screen_name
-                    })
-                )
-                .catch(console.error);
-    }
+    await client
+        .getAccessToken({
+            oauth_verifier: oauthVerifier,
+            oauth_token: oauthToken
+        })
+        .then(res => {
+            console.log({
+                accTkn: res.oauth_token,
+                accTknSecret: res.oauth_token_secret,
+                userId: res.user_id,
+                screenName: res.screen_name
+            })
+            return name = res.screen_name;
+        })
+        .catch(console.error);
 
-    render() {
-        return (
-            <div>
-                <h1>Page d'authentification</h1>
-            </div>
-        )
-    }
-
+    return { props: { name } };
 }
 
 export default Authorization;
