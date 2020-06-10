26cc70a107bebf68faaa0fb633e043651f2a3d62
Greeuh <Greeuh@users.noreply.github.com>
2020-06-10 20:05:14 +0200

    Update authorization.js

--- app/pages/authorization.js
+++ app/pages/authorization.js
@@ -4,36 +4,42 @@ import Twitter from 'twitter-lite';
 
 class Authorization extends Component {
 
-    componentDidMount (){
-    const client = new Twitter({
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    });
+    componentDidMount() {
+        const client = new Twitter({
+            consumer_key: process.env.TWITTER_CONSUMER_KEY,
+            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        });
 
-    client
-        .getAccessToken({
-            oauth_verifier: oauthVerifier,
-            oauth_token: oauthToken
-        })
-        .then(res =>
-            console.log({
-                accTkn: res.oauth_token,
-                accTknSecret: res.oauth_token_secret,
-                userId: res.user_id,
-                screenName: res.screen_name
-            })
-        )
-        .catch(console.error);
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const oauthVerifier = urlParams.get('oauth_verifier');
+        const oauthToken = urlParams.get('oauth_token');
+
+
+            client
+                .getAccessToken({
+                    oauth_verifier: oauthVerifier,
+                    oauth_token: oauthToken
+                })
+                .then(res =>
+                    console.log({
+                        accTkn: res.oauth_token,
+                        accTknSecret: res.oauth_token_secret,
+                        userId: res.user_id,
+                        screenName: res.screen_name
+                    })
+                )
+                .catch(console.error);
     }
 
-    render () {
-        return(
+    render() {
+        return (
             <div>
                 <h1>Page d'authentification</h1>
             </div>
         )
     }
-    
+
 }
 
 export default Authorization;
