d7e9483752c5ee863890bab94ba1d2dc40354206
Greeuh <Greeuh@users.noreply.github.com>
2020-06-11 15:48:19 +0200

    Add redirection to login.js

--- app/pages/login.js
+++ app/pages/login.js
@@ -4,13 +4,13 @@ import Twitter from 'twitter-lite';
 
 function Login({token}) {
 
-        let url = 'https://api.twitter.com/oauth/authorize?oauth_token=' + token
+        let url = 'https://api.twitter.com/oauth/authorize?oauth_token=' + token;
+        window.location.href = url;
 
         return (
             <div>
                 <h1>Vous allez être redirigé sur le site de Twitter...</h1>
                 <a href={url} id="link" >Ou cliquez là </a>
-                <p>Ceci est le token : {token} .</p>
             </div>);
 }
 
