19ab4df573550d9966a0b0e18fceb2b00aaeea08
Greeuh <Greeuh@users.noreply.github.com>
2020-06-17 15:01:05 +0200

    debug

--- app/pages/app.js
+++ app/pages/app.js
@@ -111,8 +111,8 @@ export async function getServerSideProps({ ctx }) {
     version: "1.1", // version "1.1" is the default (change for other subdomains)
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: cookies.UserToken,
-    access_token_secret: cookies.UserTokenSecret
+    access_token_key: cookie.UserToken,
+    access_token_secret: cookie.UserTokenSecret
 
   });
 
