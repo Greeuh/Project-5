42bf303411379f597e146c0037de6077681ded57
Greeuh <Greeuh@users.noreply.github.com>
2020-06-18 17:27:34 +0200

    fusion component & app

--- app/pages/app.js
+++ app/pages/app.js
@@ -9,7 +9,7 @@ import dynamic from 'next/dynamic';
 const QueryUser = dynamic(
   () => import('../components/queryUser.js'));
 
-function Dashboard({ results }) {
+function Dashboard({ results, userOwnTweets }) {
 
   const cookies = parseCookies();
 
@@ -37,7 +37,28 @@ function Dashboard({ results }) {
             </div>
           </div>)}
       </div>,
-      <QueryUser />
+      <div class="TweetsTimelineQueryUser">
+      {userOwnTweets.map(result =>
+          <div class="tw-block-parent">
+          <div class="timeline-TweetList-tweet">
+            <div class="timeline-Tweet">
+              <div class="timeline-Tweet-brand">
+                <div class="Icon Icon--twitter"></div>
+              </div>
+              <div class="timeline-Tweet-author">
+                <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.user.id_str}> </a><span class="TweetAuthor-avatar">
+                  <div class="Avatar"><img src={result.user.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.user.name}</span>  <span class="Icon Icon--verified"> </span> <span class="TweetAuthor-screenName">@{result.user.screen_name}</span></div>
+              </div>
+              <div class="timeline-Tweet-text">{result.text}</div>
+              <div class="timeline-Tweet-metadata"><a href={'https://twitter.com/' + result.user.screen_name + '/status/' + result.id_str}><span class="timeline-Tweet-timestamp">{result.created_at}</span></a></div>
+              <ul class="timeline-Tweet-actions">
+                <li class="timeline-Tweet-action"><a class="Icon Icon--heart" href="#"></a></li>
+                <li class="timeline-Tweet-action"><a class="Icon Icon--share" href="#"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>)}
+  </div>
     ]
   } else {
     return (
@@ -71,6 +92,7 @@ export async function getServerSideProps(ctx) {
   });
 
   let results;
+  let userOwnTweets;
 
   await client
     .get("statuses/home_timeline", {
@@ -81,46 +103,18 @@ export async function getServerSideProps(ctx) {
       return results;
     })
 
-  return { props: { results } };
-}
-
-export async function getServerSideProps(ctx) {
-
-  const cookies = parseCookies(ctx)
-
-  const client = new Twitter({
-      subdomain: "api", // "api" is the default (change for other subdomains)
-      version: "1.1", // version "1.1" is the default (change for other subdomains)
-      consumer_key: process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-      access_token_key: cookies.UserToken,
-      access_token_secret: cookies.UserTokenSecret
-  });
-
-  let results;
-  let user_id;
-
-  // await client
-  //     .get("account/verify_credentials")
-  //     .then(result => {
-  //         user_id = result.id_str;
-  //         return user_id;
-  //     })
-  //     .catch(console.error);
-
   await client
-      .get("statuses/user_timeline", {
-          user_id: 1240656557815275520,
-          count: 50,
-      })
-      .then(res => {
-          results = res;
-          return results;
-      })
+    .get("statuses/user_timeline", {
+      user_id: 1240656557815275520,
+      count: 50,
+    })
+    .then(res => {
+      results = res;
+      return userOwnTweets;
+    })
 
-  return { Component: { QueryUser } };
+  return { props: { results, userOwnTweets } };
 }
-
 
 
 class App extends Component {
