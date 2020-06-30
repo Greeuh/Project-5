317bae6b72bd3e20d5cecddc9277983887f24361
Greeuh <Greeuh@users.noreply.github.com>
2020-06-30 18:49:09 +0200

    update app.js

--- app/pages/app.js
+++ app/pages/app.js
@@ -40,48 +40,53 @@ class App extends Component {
 
     return (
       <div className="App" id="main">
+
         <div id="main-Timeline">
-        {this.state.timeline.map(result =>
-          <div class="tw-block-parent">
-            <div class="timeline-TweetList-tweet">
-              <div class="timeline-Tweet">
-                <div class="timeline-Tweet-brand">
-                  <div class="Icon Icon--twitter"></div>
+          {this.state.timeline.map(result =>
+            <div class="tw-block-parent">
+              <div class="timeline-TweetList-tweet">
+                <div class="timeline-Tweet">
+                  <div class="timeline-Tweet-brand">
+                    <div class="Icon Icon--twitter"></div>
+                  </div>
+                  <div class="timeline-Tweet-author">
+                    <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.user.id_str}> </a><span class="TweetAuthor-avatar">
+                      <div class="Avatar"><img src={result.user.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.user.name}</span>  <span class="Icon Icon--verified"> </span> <span class="TweetAuthor-screenName">@{result.user.screen_name}</span></div>
+                  </div>
+                  <div class="timeline-Tweet-text">{result.text}</div>
+                  <div class="timeline-Tweet-metadata"><a href={'https://twitter.com/' + result.user.screen_name + '/status/' + result.id_str}><span class="timeline-Tweet-timestamp">{result.created_at}</span></a></div>
+                  <ul class="timeline-Tweet-actions">
+                    <li class="timeline-Tweet-action"><a class="Icon Icon--heart" href="#"></a></li>
+                    <li class="timeline-Tweet-action"><a class="Icon Icon--share" href="#"></a></li>
+                  </ul>
                 </div>
-                <div class="timeline-Tweet-author">
-                  <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.user.id_str}> </a><span class="TweetAuthor-avatar">
-                    <div class="Avatar"><img src={result.user.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.user.name}</span>  <span class="Icon Icon--verified"> </span> <span class="TweetAuthor-screenName">@{result.user.screen_name}</span></div>
-                </div>
-                <div class="timeline-Tweet-text">{result.text}</div>
-                <div class="timeline-Tweet-metadata"><a href={'https://twitter.com/' + result.user.screen_name + '/status/' + result.id_str}><span class="timeline-Tweet-timestamp">{result.created_at}</span></a></div>
-                <ul class="timeline-Tweet-actions">
-                  <li class="timeline-Tweet-action"><a class="Icon Icon--heart" href="#"></a></li>
-                  <li class="timeline-Tweet-action"><a class="Icon Icon--share" href="#"></a></li>
-                </ul>
               </div>
-            </div>
-          </div>)}</div>,
-          <div id="userowntweets">
-        {this.state.userowntweets.map(result =>
-          <div class="tw-block-parent">
-            <div class="timeline-TweetList-tweet">
-              <div class="timeline-Tweet">
-                <div class="timeline-Tweet-brand">
-                  <div class="Icon Icon--twitter"></div>
+            </div>)}
+        </div>
+
+        <div id="userowntweets">
+          {this.state.userowntweets.map(result =>
+            <div class="tw-block-parent">
+              <div class="timeline-TweetList-tweet">
+                <div class="timeline-Tweet">
+                  <div class="timeline-Tweet-brand">
+                    <div class="Icon Icon--twitter"></div>
+                  </div>
+                  <div class="timeline-Tweet-author">
+                    <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.user.id_str}> </a><span class="TweetAuthor-avatar">
+                      <div class="Avatar"><img src={result.user.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.user.name}</span>  <span class="Icon Icon--verified"> </span> <span class="TweetAuthor-screenName">@{result.user.screen_name}</span></div>
+                  </div>
+                  <div class="timeline-Tweet-text">{result.text}</div>
+                  <div class="timeline-Tweet-metadata"><a href={'https://twitter.com/' + result.user.screen_name + '/status/' + result.id_str}><span class="timeline-Tweet-timestamp">{result.created_at}</span></a></div>
+                  <ul class="timeline-Tweet-actions">
+                    <li class="timeline-Tweet-action"><a class="Icon Icon--heart" href="#"></a></li>
+                    <li class="timeline-Tweet-action"><a class="Icon Icon--share" href="#"></a></li>
+                  </ul>
                 </div>
-                <div class="timeline-Tweet-author">
-                  <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.user.id_str}> </a><span class="TweetAuthor-avatar">
-                    <div class="Avatar"><img src={result.user.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.user.name}</span>  <span class="Icon Icon--verified"> </span> <span class="TweetAuthor-screenName">@{result.user.screen_name}</span></div>
-                </div>
-                <div class="timeline-Tweet-text">{result.text}</div>
-                <div class="timeline-Tweet-metadata"><a href={'https://twitter.com/' + result.user.screen_name + '/status/' + result.id_str}><span class="timeline-Tweet-timestamp">{result.created_at}</span></a></div>
-                <ul class="timeline-Tweet-actions">
-                  <li class="timeline-Tweet-action"><a class="Icon Icon--heart" href="#"></a></li>
-                  <li class="timeline-Tweet-action"><a class="Icon Icon--share" href="#"></a></li>
-                </ul>
               </div>
-            </div>
-          </div>)}</div>
+            </div>)}
+        </div>
+
       </div>
     );
   }
