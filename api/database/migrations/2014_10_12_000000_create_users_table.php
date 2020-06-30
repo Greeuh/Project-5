9a8037ed0143e249ccb08cb8def57c696c8e6e0e
Greeuh <Greeuh@users.noreply.github.com>
2020-06-30 14:43:46 +0200

    change user_id for id, allow usage of LoginUsingId function from laravel class auth

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->integer('user_id')->unique();
+            $table->integer('id')->unique();
             $table->string('screen_name');
             $table->string('email');
             $table->string('oauth_token');
--- api/routes/web.php
+++ api/routes/web.php
@@ -82,13 +82,13 @@ Route::get('twitter/callback', ['as' => 'twitter.callback', function() {
 
 			Session::put('access_token', $token);
 
-			if (User::where('user_id', '=', $token['user_id'])->count() > 0) {
+			if (User::where('id', '=', $token['user_id'])->count() > 0) {
 
 				Auth::loginUsingId($token['user_id']);
 
 			} else {
 				$user = new User();
-				$user->user_id = $token['user_id'];
+				$user->id = $token['user_id'];
 				$user->screen_name = $token['screen_name'];
 				$user->oauth_token = $token['oauth_token'];
 				$user->oauth_token_secret = $token['oauth_token_secret'];
