970ea3954ac06f3042150cd05fef25c19614c957
Greeuh <Greeuh@users.noreply.github.com>
2020-06-30 16:43:45 +0200

    debug

--- app/pages/app.js
+++ app/pages/app.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const axiosConfig = {
+  headers: {
+    'content-Type': 'application/json',
+    "Accept": "/",
+    "Cache-Control": "no-cache",
+    "Cookie": document.cookie
+  },
+  credentials: "same-origin"
+};
+axios.defaults.withCredentials = true;
+axios.get('/url',
+  axiosConfig)
+  .then((res) => {
+    // Some result here
+  })
+  .catch((err) => {
+    console.log(':(');
+  });
+
 class App extends Component {
   constructor(props) {
     super(props)
