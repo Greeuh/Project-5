da1a261422f0817baffdcb54787f46dcdcc8b95e
Greeuh <Greeuh@users.noreply.github.com>
2020-06-30 14:17:52 +0200

    check if user exist

--- api/routes/web.php
+++ api/routes/web.php
@@ -82,14 +82,18 @@ Route::get('twitter/callback', ['as' => 'twitter.callback', function() {
 
 			Session::put('access_token', $token);
 
-			$user = new User();
-			$user->user_id = $token['user_id'];
-			$user->screen_name = $token['screen_name'];
-			$user->oauth_token = $token['oauth_token'];
-			$user->oauth_token_secret = $token['oauth_token_secret'];
-			$user->save();
+			if (User::where('user_id', '=', $token['user_id'])->count() > 0) {
+				Auth::login($user, true);
+			} else {
+				$user = new User();
+				$user->user_id = $token['user_id'];
+				$user->screen_name = $token['screen_name'];
+				$user->oauth_token = $token['oauth_token'];
+				$user->oauth_token_secret = $token['oauth_token_secret'];
+				$user->save();
 
-			Auth::login($user, true);
+				Auth::login($user, true);
+			}
 
 			return Redirect::to('https://projet5ocr.antoineparriaud.fr:3000/app')->with('flash_notice', 'Congrats! You\'ve successfully signed in!');
 		}
