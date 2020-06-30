478d83c520cb1fd4d2ac11a5bf1525333c2e1656
Greeuh <Greeuh@users.noreply.github.com>
2020-06-30 12:45:56 +0200

    create users table

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->user_id()->unique();
+            $table->string('screen_name');
+            $table->string('email');
+            $table->oauth_token();
+            $table->oauth_token_secret();
             $table->timestamps();
         });
     }
