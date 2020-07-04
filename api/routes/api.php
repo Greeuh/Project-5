825ac2c40a649bc22bc410c1e9527da59a96208f
Greeuh <45976968+Greeuh@users.noreply.github.com>
2020-07-04 12:40:01 +0200

    linkify

--- api/routes/api.php
+++ api/routes/api.php
@@ -38,8 +38,8 @@ Route::get('/homeTimeline', function (Request $request) {
 
     $tweetRes = array();
 
-    foreach ($tweets as &$full_text) {
-        $full_text = Twitter::linkify($full_text);
+    foreach ($tweets as &$tweet.full_text) {
+        $tweet.full_text = Twitter::linkify($tweet.full_text);
         // $tweetRes = $tweetLinkified;
     }
 
