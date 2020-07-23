b626f8db98ca405503577a628fc18ac832628c38
Greeuh <Greeuh@users.noreply.github.com>
2020-07-23 17:57:46 +0200

    trying to fix route for component

--- app/pages/app.js
+++ app/pages/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import PostTweet from './components/PostTweet';
+import PostTweet from '/components/PostTweet';
 
 class App extends Component {
   constructor(props) {
