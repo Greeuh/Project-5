cc1e1ca7700b4d9c714345b0daf0b5432eceef23
Greeuh <Greeuh@users.noreply.github.com>
2020-09-02 23:00:51 +0200

    release DmTimeline

--- app/components/DmTimeline.js
+++ app/components/DmTimeline.js
@@ -7,7 +7,7 @@ export default class DmTimeline extends React.Component {
     }
 
     render() {
-        if (this.props.data) {
+        if (this.props.data.events) {
             return <div>
                 {
                     this.props.data.events.map(result =>
@@ -18,11 +18,11 @@ export default class DmTimeline extends React.Component {
                                         <div class="Icon Icon--twitter"></div>
                                     </div>
                                     <div class="timeline-Tweet-author">
-                                        <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.events.message_create.sender_id.id_str}> </a><span class="TweetAuthor-avatar">
-                                            <div class="Avatar"><img src={result.events.message_create.sender_id.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.events.message_create.sender_id.name}</span>  <span class="Icon Icon--verified"> </span> <span class="TweetAuthor-screenName">@{result.events.message_create.sender_id.screen_name}</span></div>
+                                        <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.message_create.sender_id.id_str}> </a><span class="TweetAuthor-avatar">
+                                            <div class="Avatar"><img src={result.message_create.sender_id.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.message_create.sender_id.name}</span>  <span class="Icon Icon--verified"> </span> <span class="TweetAuthor-screenName">@{result.message_create.sender_id.screen_name}</span></div>
                                     </div>
-                                    <div class="timeline-Tweet-text" dangerouslySetInnerHTML={{ __html: result.events.message_create.message_data.text }} />
-                                    <div class="timeline-Tweet-metadata"><span class="timeline-Tweet-timestamp">{result.events.created_timestamp}</span></div>
+                                    <div class="timeline-Tweet-text" dangerouslySetInnerHTML={{ __html: result.message_create.message_data.text }} />
+                                    <div class="timeline-Tweet-metadata"><span class="timeline-Tweet-timestamp">{result.created_timestamp}</span></div>
                                     <ul class="timeline-Tweet-actions">
                                         <li class="timeline-Tweet-action">
                                             <a class="Icon Icon--hearted"></a>
