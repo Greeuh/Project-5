523fae6325ed4f9f7976775d449e8c5fbfd0227b
Greeuh <Greeuh@users.noreply.github.com>
2020-09-03 16:52:57 +0200

    add refresh when fav/rt in this TL

--- app/components/QueryUser.js
+++ app/components/QueryUser.js
@@ -48,7 +48,7 @@ export default class QueryUser extends React.Component {
         })
             .then(response => {
                 console.log(response);
-                this.props.refreshT();
+                this.refreshThisTimeline();
             })
             .catch(error => {
                 console.log(error);
@@ -63,7 +63,7 @@ export default class QueryUser extends React.Component {
         })
             .then(response => {
                 console.log(response);
-                this.props.refreshT();
+                this.refreshThisTimeline();
             })
             .catch(error => {
                 console.log(error);
@@ -78,7 +78,7 @@ export default class QueryUser extends React.Component {
         })
             .then(response => {
                 console.log(response);
-                this.props.refreshT();
+                this.refreshThisTimeline();
             })
             .catch(error => {
                 console.log(error);
