c0146ee1f7ad965b48260fcdb481fb278b7c0482
Greeuh <Greeuh@users.noreply.github.com>
2020-09-09 16:45:03 +0200

    using fontawesome React

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,6 +1,5 @@
 import './timeline.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '/css/all.css';
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
--- app/pages/login.js
+++ app/pages/login.js
@@ -1,9 +1,12 @@
+import { faTwitter } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Login() {
     return (
         <div class="container">
             <div class="center">
                 <a id="twitter-button" class="btn btn-block btn-social btn-twitter" href="https://projet5ocr.antoineparriaud.fr/twitter/login">
-                    <i class="fa fa-twitter"></i> Sign in with Twitter
+                <FontAwesomeIcon icon={faTwitter} /> Sign in with Twitter
                 </a>
             </div>
         </div>
