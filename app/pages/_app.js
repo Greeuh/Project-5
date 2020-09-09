db65b082ca7bde5caee6479e11f3cfe0b382c5b5
Greeuh <Greeuh@users.noreply.github.com>
2020-09-09 16:19:19 +0200

    fix _app

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,8 +1,5 @@
 import './timeline.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap-theme.min.css';
-
-
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
