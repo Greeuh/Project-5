0432925d439715557ec7a191242c704fdf3e8b9a
Greeuh <Greeuh@users.noreply.github.com>
2020-09-09 16:55:34 +0200

    debug

--- app/pages/app.js
+++ app/pages/app.js
@@ -22,7 +22,7 @@ class App extends Component {
   componentDidMount() {
     const cookies = parseCookies();
 
-    if (!cookies.user_id) {
+    if (cookies.user_id === 'null') {
       return window.location.replace("https://projet5ocr.antoineparriaud.fr:3000/login");
     } else {
 
