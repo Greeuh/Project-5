8358888729a3a2ee01900e488b4f1aff3a7b74eb
Greeuh <Greeuh@users.noreply.github.com>
2020-09-09 16:18:41 +0200

    move bootstrap to _app

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,4 +1,8 @@
 import './timeline.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.min.css';
+
+
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
--- app/pages/login.js
+++ app/pages/login.js
@@ -1,6 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap-theme.min.css';
-
 export default function Login() {
     return (
         <div class="container">
