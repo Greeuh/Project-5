06ce950a4626936ff1b812bcdab44214caae09f1
Greeuh <Greeuh@users.noreply.github.com>
2020-09-10 16:06:52 +0200

    debug

--- app/components/QueryUser.js
+++ app/components/QueryUser.js
@@ -8,7 +8,7 @@ export default class QueryUser extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: undefined,
+            value: '',
             queryUserT: undefined,
         };
         this.setState({ value: this.props.user});
