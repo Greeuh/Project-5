ba0ff6b3b11b53ec4b6273a929d62c66df5562f3
Greeuh <Greeuh@users.noreply.github.com>
2020-09-17 14:59:39 +0200

    work on RT view

--- app/components/TimelineColumn.js
+++ app/components/TimelineColumn.js
@@ -78,28 +78,17 @@ export default class TimelineColumn extends React.Component {
                                 ? <div class="timeline-TweetList-tweet">
                                     <div class="timeline-Tweet">
                                         <div class="timeline-Tweet-brand">
-                                            <div class="Icon Icon--twitter"></div>
+                                            <div class="Icon Icon--twitter"><span class="TweetAuthor-name">{result.user.name} retweet :</span></div>
                                         </div>
                                         <div class="timeline-Tweet-author">
-                                            <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.user.id_str}> </a><span class="TweetAuthor-avatar">
-                                                <div class="Avatar"><img src={result.user.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.user.name}</span>
-                                                {result.user.verified
+                                            <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.retweeted_status.user.id_str}> </a><span class="TweetAuthor-avatar">
+                                                <div class="Avatar"><img src={result.retweeted_status.user.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.retweeted_status.user.name}</span>
+                                                {result.retweeted_status.user.verified
                                                     ? <span class="Icon Icon--verified"> </span>
                                                     : ''}
-                                                <span class="TweetAuthor-screenName">@{result.user.screen_name} </span>
-                                            retweet :</div>
+                                                <span class="TweetAuthor-screenName">@{result.retweeted_status.user.screen_name} </span></div>
                                         </div>
-                                        <div class="timeline-Tweet-text">
-                                            <div class="timeline-Tweet-author">
-                                                <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.retweeted_status.user.id_str}> </a><span class="TweetAuthor-avatar">
-                                                    <div class="Avatar"><img src={result.retweeted_status.user.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.retweeted_status.user.name}</span>
-                                                    {result.retweeted_status.user.verified
-                                                        ? <span class="Icon Icon--verified"> </span>
-                                                        : ''}
-                                                    <span class="TweetAuthor-screenName">@{result.retweeted_status.user.screen_name} </span></div>
-                                            </div>
-                                    dangerouslySetInnerHTML={{ __html: result.retweeted_status.full_text }}
-                                        </div>
+                                        <div class="timeline-Tweet-text" dangerouslySetInnerHTML={{ __html: result.retweeted_status.full_text }} />
                                         <div class="timeline-Tweet-metadata"><a href={'https://twitter.com/' + result.user.screen_name + '/status/' + result.id_str}><span class="timeline-Tweet-timestamp">{result.created_at}</span></a></div>
                                         <ul class="timeline-Tweet-actions">
                                             <li class="timeline-Tweet-action"> {result.favorited
