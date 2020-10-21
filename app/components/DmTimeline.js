ceeaaf55cbb5e9f3c5c87dcef35b94ec85d5a46a
Greeuh <Greeuh@users.noreply.github.com>
2020-10-21 18:05:46 +0200

    Use fade for toggle DM TL

--- app/components/DmTimeline.js
+++ app/components/DmTimeline.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Fade from 'react-bootstrap/Fade';
 
 export default class DmTimeline extends React.Component {
     constructor(props) {
@@ -10,43 +9,41 @@ export default class DmTimeline extends React.Component {
     }
 
     render() {
+        const [open, setOpen] = useState(false);
         if (this.props.data) {
-            return <Accordion>
-                <Card>
-                    <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                            Show DM for the last 30 days
-                </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="0">
-                        <Card.Body><div class="timeline">
-                            {
-                                this.props.data.map(result =>
-                                    <div class="tw-block-parent">
-                                        <div class="timeline-TweetList-tweet">
-                                            <div class="timeline-Tweet">
-                                                <div class="timeline-Tweet-brand">
-                                                    <div class="Icon Icon--twitter"></div>
-                                                </div>
-                                                <div class="timeline-Tweet-author">
-                                                    <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.message_create.sender_id.id_str}> </a><span class="TweetAuthor-avatar">
-                                                        <div class="Avatar"><img src={result.message_create.sender_id.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.message_create.sender_id.name}</span>  <span class="Icon Icon--verified"> </span> <span class="TweetAuthor-screenName">@{result.message_create.sender_id.screen_name}</span></div>
-                                                </div>
-                                                <div class="timeline-Tweet-text" dangerouslySetInnerHTML={{ __html: result.message_create.message_data.text }} />
-                                                <div class="timeline-Tweet-metadata"><span class="timeline-Tweet-timestamp">{result.created_timestamp}</span></div>
-                                                <ul class="timeline-Tweet-actions">
-                                                    <li class="timeline-Tweet-action">
-                                                        <a class="Icon Icon--hearted"></a>
-                                                        <a class="Icon Icon--heart"></a>
-                                                    </li>
-                                                </ul>
-                                            </div>
+            return <div class="timeline"><Button
+                onClick={() => setOpen(!open)}
+                aria-controls="fade-dm"
+                aria-expanded={open}
+            >
+                Show your DM for the last 30 days
+          </Button>
+                <Fade in={open}>
+                    {
+                        this.props.data.map(result =>
+                            <div class="tw-block-parent">
+                                <div class="timeline-TweetList-tweet">
+                                    <div class="timeline-Tweet">
+                                        <div class="timeline-Tweet-brand">
+                                            <div class="Icon Icon--twitter"></div>
                                         </div>
-                                    </div>)
-                            }</div></Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
+                                        <div class="timeline-Tweet-author">
+                                            <div class="TweetAuthor"><a class="TweetAuthor-link" href={"?ID=" + result.message_create.sender_id.id_str}> </a><span class="TweetAuthor-avatar">
+                                                <div class="Avatar"><img src={result.message_create.sender_id.profile_image_url_https}></img> </div></span><span class="TweetAuthor-name">{result.message_create.sender_id.name}</span>  <span class="Icon Icon--verified"> </span> <span class="TweetAuthor-screenName">@{result.message_create.sender_id.screen_name}</span></div>
+                                        </div>
+                                        <div class="timeline-Tweet-text" dangerouslySetInnerHTML={{ __html: result.message_create.message_data.text }} />
+                                        <div class="timeline-Tweet-metadata"><span class="timeline-Tweet-timestamp">{result.created_timestamp}</span></div>
+                                        <ul class="timeline-Tweet-actions">
+                                            <li class="timeline-Tweet-action">
+                                                <a class="Icon Icon--hearted"></a>
+                                                <a class="Icon Icon--heart"></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>)
+                    }</Fade>
+            </div>
         } else {
             return <p>to be load</p>
         }
