d6935749f1b6415b299f55a017d316bb1044f1cc
Greeuh <Greeuh@users.noreply.github.com>
2020-11-20 18:45:15 +0100

    reverse head

--- app/pages/login.js
+++ app/pages/login.js
@@ -4,10 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Login() {
     return (
         <div className="container">
-            <Head>
-                <title>Project 5 : Tweetdeck - Login</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
             <div className="center">
                 <a id="twitter-button" className="btn btn-block btn-social btn-twitter" href="https://projet5ocr.antoineparriaud.fr/twitter/login">
                     <FontAwesomeIcon icon={faTwitter} /> Sign in with Twitter
