9950b446046ed2521d74bae02236394440b7c002
Greeuh <Greeuh@users.noreply.github.com>
2020-11-24 20:06:07 +0100

    add effect to button on login.js

--- app/pages/login.js
+++ app/pages/login.js
@@ -15,7 +15,7 @@ export default function Login() {
                 <meta name="author" content="Antoine Parriaud" />
             </Head>
             <div className="center">
-                <a id="twitter-button" className="btn btn-block btn-social btn-twitter" href="https://projet5ocr.antoineparriaud.fr/twitter/login">
+                <a id="twitter-button" className="btn btn-block btn-social btn-twitter glow-on-hover" href="https://projet5ocr.antoineparriaud.fr/twitter/login">
                     <FontAwesomeIcon icon={faTwitter} /> Sign in with Twitter
                 </a>
             </div>
